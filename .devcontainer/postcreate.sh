f560589aad13c26e0cc741542cdf7270878fae0a
Matthew Lipper <mlipper@gmail.com>
2023-11-29 18:03:38 +0000

    Adjust postcreate.sh to check before adding to .bashrc.

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 
-echo "set -o vi" >> $HOME/.bashrc \
-    && echo "alias ll=\"ls -l\"" >> $HOME/.bashrc \
-    && echo "alias lla=\"ls -lsa\"" >> $HOME/.bashrc \
-    && echo "alias c=clear" >> $HOME/.bashrc
+add_cmds=$(cat <<ADDITIONAL_COMMMANDS
+set -o vi
+alias ll='ls -l'
+alias lla='ls -lsa'
+alias c=clear
+ADDITIONAL_COMMMANDS
+)
+
+# Add additional commands unless they are already there.
+if ! egrep -q "${add_cmds}" "${HOME}/.bashrc"; then
+    echo "${add_cmds}" >> "${HOME}/.bashrc"
+fi
 
 /opt/geosupport/current/bin/geosupport install
-
