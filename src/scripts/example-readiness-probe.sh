400b1953145cb0fe428c5c67eeb1bfa37108b1c7
Matthew Lipper <mlipper@gmail.com>
2025-04-03 16:36:21 +0000

    Clean up and document script.

--- src/scripts/example-readiness-probe.sh
+++ src/scripts/example-readiness-probe.sh
@@ -1,15 +1,27 @@
 #!/usr/bin/env bash
 
-rc="$(curl -k -s --get \
-    'https://maps.nyc.gov/geoclient/v2/address?houseNumber=280&street=rsd&borough=manhattan' \
-    | jq -M -cr '.address | [.geosupportReturnCode,.geosupportReturnCode2]' | sed -r 's/(\[|,|"|\])//g')" \
-    && [[ "$rc" =~ 0[0-1]0[0-1] ]];
-#rc="$(echo '["00","01"]' | sed -r 's/(\[|,|"|\])//g')"
-#echo "$rc"
-#if [[ "$rc" =~ 0[0-1]0[0-1] ]]; then
-#    echo true
-#else
-#    echo false
-#fi
+#
+# Example Kubernetes readiness probe which executes a F1B call to
+# to Geosupport.
+#
+# If there is an issue with one of the Geosupport files, one of
+# the return code (F1EX, F1AX) will usually not match the regex
+# test, preventing the container from being considered ready.
+#
+# This example uses environment variables and a complete URL for demo
+# purposes.
+#
+# In an actual Deployment manifest, only the URI path is used and no
+# auth key is needed and the URI path would simply be:
+#
+# /geoclient/v2/address?houseNumber=280&street=rsd&borough=manhattan
+#
 
-echo $?
+BASE="${BASE:-https://api.nyc.gov/geoclient/v2}"
+KEY="${KEY:-keynotset}"
+
+rc=$(curl -k -s --get \
+    "${BASE}/address?houseNumber=280&street=rsd&borough=manhattan&key=${KEY}" \
+    | jq -M -cr '.address | [.geosupportReturnCode,.geosupportReturnCode2]' \
+    | sed -r 's/(\[|,|"|\])//g') \
+    && [[ "$rc" =~ 0[0-1]0[0-1] ]];
