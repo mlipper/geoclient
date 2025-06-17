482fb61ed2062c6b52140fe4245c3dfcfbdfe71d
Matthew Lipper <mlipper@gmail.com>
2025-06-17 14:31:14 -0400

    Remove local libs dir.

--- src/main/java/gov/nyc/doitt/gis/geoclient/json/Configuration.java
+++ src/main/java/gov/nyc/doitt/gis/geoclient/json/Configuration.java
@@ -1,8 +1,26 @@
 package gov.nyc.doitt.gis.geoclient.json;
 
+import com.fasterxml.jackson.databind.JsonNode;
+
+import gov.nyc.doitt.gis.geoclient.config.GeosupportConfig;
 
 public class Configuration {
-    public boolean someLibraryMethod() {
-        return true;
+    private static final String DEFAULT_CONFIG_FILE = "geoclient.json";
+    private String configFile;
+    
+    public Configuration(String configFile) {
+        this.configFile = configFile;
+    }
+
+    public Configuration() {
+        this(DEFAULT_CONFIG_FILE);
+    } 
+
+    public GeosupportConfig loadGeosupportConfig() {
+        return null;
+    }
+
+    protected JsonNode loadConfig(String configFile) {
+        return null;
     }
 }
