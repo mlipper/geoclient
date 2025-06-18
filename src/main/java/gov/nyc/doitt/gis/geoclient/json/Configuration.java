5e5032c885571979319a13c18a40d7ba50334916
Matthew Lipper <mlipper@gmail.com>
2025-06-18 11:19:22 -0400

    Working loadFilters test.

--- src/main/java/gov/nyc/doitt/gis/geoclient/json/Configuration.java
+++ src/main/java/gov/nyc/doitt/gis/geoclient/json/Configuration.java
@@ -1,15 +1,25 @@
 package gov.nyc.doitt.gis.geoclient.json;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import gov.nyc.doitt.gis.geoclient.config.GeosupportConfig;
+import gov.nyc.doitt.gis.geoclient.function.Filter;
 
 public class Configuration {
+    private static final Logger log = LoggerFactory.getLogger(Configuration.class);
     private static final String DEFAULT_CONFIG_FILE = "geoclient.json";
-    private String configFile;
     
+    private final JsonNode jsonNode;
+
     public Configuration(String configFile) {
-        this.configFile = configFile;
+        this.jsonNode = loadConfig(configFile);
     }
 
     public Configuration() {
@@ -20,7 +30,36 @@ public class Configuration {
         return null;
     }
 
+    public List<Filter> loadFilters() {
+        List<JsonNode> filters = this.jsonNode.findValues("filter");
+        List<Filter> result = new ArrayList<>(filters.size());
+        for (JsonNode filterNode : filters) {
+            // filterNode is an ObjectNode
+            filterNode.iterator().forEachRemaining(e -> 
+                e.properties().forEach(kv -> {
+                    if (kv.getKey().equals("pattern")) {
+                        result.add(new Filter(kv.getValue().asText()));
+                    }
+                })
+            );
+        }
+        return result;
+    }
+
+    protected ObjectMapper getObjectMapper() {
+        return new ObjectMapper();
+    }
+
     protected JsonNode loadConfig(String configFile) {
-        return null;
+        ObjectMapper mapper = getObjectMapper();
+        try (var is = getClass().getClassLoader().getResourceAsStream("geoclient.json")) {
+            //Map<String, String> result = mapper.readValue(is, new com.fasterxml.jackson.core.type.TypeReference<Map<String, String>>() {});
+            JsonNode result = mapper.readTree(is);
+            log.info("Configuration loaded: {}", result);
+            return result;
+        } catch (java.io.IOException | NullPointerException e) {
+            log.error("Failed to load JSON configuration", e);
+            throw new JsonConfigurationException("Could not load Geoclient JSON configuration", e);
+        }
     }
 }
--- src/test/java/gov/nyc/doitt/gis/geoclient/json/ConfigurationTest.java
+++ src/test/java/gov/nyc/doitt/gis/geoclient/json/ConfigurationTest.java
@@ -1,30 +1,33 @@
 package gov.nyc.doitt.gis.geoclient.json;
 
 
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assumptions.assumeFalse;
 
 import java.io.IOException;
-import java.util.Map;
+import java.util.List;
+//import java.util.Map;
 
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import gov.nyc.doitt.gis.geoclient.function.Filter;
 
 class ConfigurationTest {
 
     private static final Logger log = LoggerFactory.getLogger(ConfigurationTest.class);
 
-    @SuppressWarnings("unchecked")
     @Test
-    void jacksonBasic() throws IOException {
-        ObjectMapper mapper = new ObjectMapper();
-        try (var is = getClass().getClassLoader().getResourceAsStream("geoclient.json")) {
-            assertNotNull(is, "config.json not found in classpath");
-            Map<String, String> result = mapper.readValue(is, Map.class);
-            log.info("Configuration loaded: {}", result);
-            assertNotNull(result);
+    void test_loadFilters() throws IOException {
+        Configuration config = new Configuration();
+        List<Filter> filters = config.loadFilters();
+        log.info("filters: {}", filters);
+        assertNotNull(filters);
+        assumeFalse(filters.isEmpty());
+        for (Filter filter : filters) {
+            assertNotEquals("Filter [pattern=missing]", filter.toString());
         }
     }
 }
