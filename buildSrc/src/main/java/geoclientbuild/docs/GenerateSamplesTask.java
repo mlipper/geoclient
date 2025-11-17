da7377c79c281d440700d8d410a37c45717492de
Matthew Lipper <mlipper@gmail.com>
2025-11-17 21:03:30 +0000

    Remove unused import.

--- buildSrc/src/main/java/geoclientbuild/docs/GenerateSamplesTask.java
+++ buildSrc/src/main/java/geoclientbuild/docs/GenerateSamplesTask.java
@@ -13,7 +13,6 @@ import org.gradle.api.DefaultTask;
 import org.gradle.api.file.DirectoryProperty;
 import org.gradle.api.file.RegularFile;
 import org.gradle.api.file.RegularFileProperty;
-import org.gradle.api.provider.MapProperty;
 import org.gradle.api.provider.Property;
 import org.gradle.api.tasks.Input;
 import org.gradle.api.tasks.InputFile;
